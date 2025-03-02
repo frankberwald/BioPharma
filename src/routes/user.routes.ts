41e1d09f530e59f951c529a645e0fd5e9a633681
frankberwald <berwald1997@outlook.com>
2025-03-02 12:58:42 -0300

    user routes fixed

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,29 @@
-import { Router } from "express";
+import { NextFunction, Router } from "express";
 import { UserController } from "../controllers/UserController";
+import { Request, Response } from "express"
 
 const userController = new UserController()
 
 const userRouter = Router();
 
-userRouter.post('/', userController.createUser)
+userRouter.post("/", (req: Request, res: Response) => {
+  userController.createUser(req, res);
+});
 
-userRouter.get('/', userController.getAll)
+userRouter.get('/', (req: Request, res: Response) => {
+  userController.getAll
+})
 
-userRouter.get('/:id', userController.getById)
+userRouter.get('/:id', (req: Request, res: Response) => {
+  userController.getById(req, res)
+})
 
-userRouter.put('/:id', userController.userUpdate)
+userRouter.put('/:id', (req: Request, res: Response, next: NextFunction) => {
+  userController.userUpdate(req, res, next)
+})
 
-userRouter.patch('/:id/status', userController.updateStatus)
+userRouter.patch("/:id/status", (req: Request, res: Response, next: NextFunction) => {
+  userController.updateStatus(req, res, next);
+});
 
 export default userRouter;
